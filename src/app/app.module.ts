500dc8eb55352f201a8896c52bbf60f886b0bc9d
aljimenez2 <lagaleria17@gmail.com>
2018-01-17 17:13:49 -0300

    adding class 46

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+//Rutas
+import { APP_ROUTING } from './app.routes';
 
+//Servicios
 
-import { AppComponent } from './app.component';
+//Componentes
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutComponent,
+        HeroesComponent
+    ],
+    imports: [
+        BrowserModule,
+        APP_ROUTING
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
